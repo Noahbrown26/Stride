46449079705b9e1e4d0b4ea99d68dfd3d9fb8062
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-12-06 19:53:22 -0800

    contributing to navbar/package

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,14 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Navbar/>
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
   );
 }
 

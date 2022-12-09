174760f947a176c28014019f192ebc07d262fd9a
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-12-08 20:06:57 -0800

    adding functions to home and detail.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
-const home = () => {
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
-    <div>home</div>
-  )
-}
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
 
-export default home
+export default Home;

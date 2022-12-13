e8d26e1eb95c3631af1e8084bd4e89d7c397ad2b
Noahb26 <noahbrown2663@gmail.com>
2022-12-12 20:12:24 -0800

    add login/register jsx via @luisvilla

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 //import Footer from './components/Footer';
+import Login from './pages/login.jsx';
+import Register from './pages/register.jsx';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/Register' component={Register} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>

6976d2b7a66c9dd95341a70830e161b5b89a221a
Noahb26 <noahbrown2663@gmail.com>
2022-12-15 21:54:40 -0800

    update login/signup modal & setup apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -10,20 +10,52 @@ import Login from './pages/login.jsx';
 import Register from './pages/register.jsx';
 import Workouts from './pages/workouts.jsx';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/Register' component={Register} />
           <Route exact path='/Workouts' component={Workouts} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
